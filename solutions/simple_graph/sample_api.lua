58546d9ce17f91ea5540fccc2f14d51f1a74088b
Mike Aanenson <mikeaanenson@exosite.com>
2016-06-27 11:21:59 -0500

    a few tweaks to API handlers

--- solutions/simple_graph/sample_api.lua
+++ solutions/simple_graph/sample_api.lua
@@ -1,13 +1,24 @@
 --#ENDPOINT GET /admin/lightbulb/{sn}
 local sn = tostring(request.parameters.sn)
+local window = tostring(request.parameters.window)
 
 if true then
   local data = {}
+  if window == nil then window = '30' end
   out = Timeseries.query({
     epoch='ms',
-    q = 'SELECT value FROM temperature,humidity,'.. sn ..' WHERE time > now() - 30m LIMIT 10000'})
+    q = "SELECT value FROM temperature,humidity WHERE sn = '" ..sn.."' AND time > now() - "..window.."m LIMIT 10000"})
   data['timeseries'] = out
   return data
 else
   http_error(403, response)
 end
+--#ENDPOINT GET /admin/test
+return 'running'
+--#ENDPOINT GET /admin/gettsdb
+local data = {}
+out = Timeseries.query({
+  epoch='ms',
+  q = 'SELECT value FROM *'})
+data['timeseries'] = out
+return data
