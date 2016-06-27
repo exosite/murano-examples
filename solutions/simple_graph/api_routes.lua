d0e96f027e9c74bc90b141e9f030da143a4b40b4
Mike Aanenson <mikeaanenson@exosite.com>
2016-06-27 16:28:18 -0500

    changing to use term 'identifier' instead of 'sn'.

--- solutions/simple_graph/api_routes.lua
+++ solutions/simple_graph/api_routes.lua
@@ -1,8 +1,16 @@
 --#ENDPOINT GET /development/test
-return 'hello world! \r\nI am a test Murano Solution API Route entpoint'
+return 'Hello World! \r\nI am a test Murano Solution API Route entpoint'
 
---#ENDPOINT GET /development/device/{sn}/data
-local sn = tostring(request.parameters.sn)
+--#ENDPOINT GET /development/device/{sn}/keyvalue
+-- Show current key-value data for a specific unique device
+local identifier = tostring(request.parameters.identifier)
+local resp = Keystore.get({key = "identifier_" .. identifier})
+return 'Getting Key Value Raw Data for: '..identifier..'\r\n'..to_json(resp)
+
+
+--#ENDPOINT GET /development/device/{identifier}/timeseries
+-- Show current time-series data for a specific unique device
+local identifier = tostring(request.parameters.identifier)
 local window = tostring(request.parameters.window) -- in minutes,if ?window=<number>
 if true then
   local data = {}
@@ -10,7 +18,25 @@ if true then
   -- Assumes temperature and humidity data device resources
   out = Timeseries.query({
     epoch='ms',
-    q = "SELECT value FROM temperature,humidity WHERE sn = '" ..sn.."' AND time > now() - "..window.."m LIMIT 10000"})
+    q = "SELECT value FROM temperature,humidity WHERE identifier = '" ..identifier.."' LIMIT 20"})
+  data['timeseries'] = out
+
+  return 'Getting Last 20 Time Series Raw Data Points for: '..identifier..'\r\n'..to_json(out)
+else
+  http_error(403, response)
+end
+
+
+--#ENDPOINT GET /development/device/{identifier}/data
+local identifier = tostring(request.parameters.identifier)
+local window = tostring(request.parameters.window) -- in minutes,if ?window=<number>
+if true then
+  local data = {}
+  if window == nil then window = '30' end
+  -- Assumes temperature and humidity data device resources
+  out = Timeseries.query({
+    epoch='ms',
+    q = "SELECT value FROM temperature,humidity WHERE identifier = '" ..identifier.."' AND time > now() - "..window.."m LIMIT 10000"})
   data['timeseries'] = out
   return data
 else
--- solutions/simple_graph/service_handlers/product.lua
+++ solutions/simple_graph/service_handlers/product.lua
@@ -1,20 +1,25 @@
 --
 -- Product Event Handler
---
--- TIME SERIES DATABASE STORAGE:
+-- Handle data events
+
+-- PUT DATA INTO TIME SERIES DATABASE STORAGE:
+-- ============================
 -- Write All Device Resource Data to timeseries database
 Timeseries.write({
-  query = data.alias .. ",sn=" .. data.device_sn .. " value=" .. data.value[2]
+  query = data.alias .. ",identifier=" .. data.device_sn .. " value=" .. data.value[2]
 })
 
--- KEY VALUE STORE:
+-- PUT DATA INTO KEY VALUE STORE:
+-- ============================
 -- Write All Device Resources incomiong data to key/value data store
 -- Check to see what data already exists
-local resp = Keystore.get({key = "sn_" .. data.device_sn})
+local resp = Keystore.get({key = "identifier_" .. data.device_sn})
+
 -- Make sure each device has the following keys stored
 local value = {
   temperature = "undefined",
-  hours = "undefined",
+  humidity = "undefined",
+  uptime = "undefined",
   state = "undefined"
 }
 if type(resp) == "table" and type(resp.value) == "string" then
@@ -22,8 +27,7 @@ if type(resp) == "table" and type(resp.value) == "string" then
 end
 -- Add in other available data about this device / incoming data
 value[data.alias] = data.value[2]
-value["timestamp"] = data.timestamp/1000
+value["timestamp"] = data.timestamp/1000 -- add server's timestamp
 value["pid"] = data.vendor or data.pid
-value["ip"] = data.source_ip
 -- Write data into key/value data store
-Keystore.set({key = "sn_" .. data.device_sn, value = to_json(values)})
+Keystore.set({key = "identifier_" .. data.device_sn, value = to_json(value)})
